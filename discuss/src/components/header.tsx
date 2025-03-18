8dfd1ee4365118014100a217d377674472907c56
Danish Ali <dali27363@gmail.com>
2025-03-18 16:22:04 +0500

    Add Radix UI Avatar component and input styling; update package dependencies

---
+++ discuss/src/components/header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+        <div>
+            <h1 className="font-bold text-xl">Discuss</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Header
