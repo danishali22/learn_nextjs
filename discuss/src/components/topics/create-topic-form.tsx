f5eea65913fb6f6512a9653dc347ce364fcf3f49
Danish Ali <dali27363@gmail.com>
2025-03-21 17:21:11 +0500

    Implement create topic form with validation and error handling; add topic show page and textarea component

--- discuss/src/components/topics/create-topic-form.tsx
+++ discuss/src/components/topics/create-topic-form.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -10,37 +12,53 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Textarea } from "../ui/textarea";
+import { createTopic } from "@/actions/create-topic";
+import { useActionState } from "react";
 
 export function CreateTopicForm() {
+  const [formState, action] = useActionState(createTopic, {errors: {}});
   return (
     <Dialog>
       <DialogTrigger asChild>
         <Button className="cursor-pointer">New Topic</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Edit profile</DialogTitle>
-          <DialogDescription>
-            Make changes to your profile here. Click save when you&apos;re done.
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              Name
-            </Label>
-            <Input id="name" value="Pedro Duarte" className="col-span-3" />
+        <form action={action}>
+          <DialogHeader>
+            <DialogTitle>Create a Topic</DialogTitle>
+            <DialogDescription>
+              Write a new topic to start a discussion. Click save when
+              you&apos;re done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4 py-4">
+            <div>
+              <Label htmlFor="name">Name</Label>
+              <Input id="name" name="name" />
+            </div>
+            {formState.errors.name && (
+              <p className="text-sm text-red-500">{formState.errors.name}</p>
+            )}
+            <div>
+              <Label htmlFor="description">Description</Label>
+              <Textarea id="description" name="description" />
+            </div>
+            {formState.errors.description && (
+              <p className="text-sm text-red-500">
+                {formState.errors.description}
+              </p>
+            )}
+            {formState.errors.formError && (
+              <div className="border border-red-500 bg-red-200">
+                {formState.errors.formError}
+              </div>
+            )}
           </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="username" className="text-right">
-              Username
-            </Label>
-            <Input id="username" value="@peduarte" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">Save changes</Button>
-        </DialogFooter>
+          <DialogFooter>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
     </Dialog>
   );
